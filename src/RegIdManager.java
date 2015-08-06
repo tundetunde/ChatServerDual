3181f92912fd5d1065a6f612bcb8edc39d4f9c1c
emp-daisy <adajess01@gmail.com>
2015-08-06 18:50:45 +0100

    DB added

--- src/RegIdManager.java
+++ src/RegIdManager.java
@@ -1,72 +1,57 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
+import java.sql.*;
 
 public class RegIdManager {
 	static final String REG_ID_STORE = "GCMRegId.txt";
-	public static void writeToFile(String regId) throws IOException {
-		Set<String> regIdSet = null;
-		createFile();
-		try{
-			regIdSet = readFromFile();
-		}catch(FileNotFoundException e){
-			createFile();
-		}catch(IOException e){
-			createFile();
-		}
-		
-
-		if (!regIdSet.contains(regId)) {
-			PrintWriter out = new PrintWriter(new BufferedWriter(
-					new FileWriter(REG_ID_STORE, true)));
-			out.println(regId);
-			out.close();
-		}
-	}
-
-	public static Set<String> readFromFile() throws IOException, FileNotFoundException{
-		BufferedReader br = null;
-		try{
-			br = new BufferedReader(new FileReader(REG_ID_STORE));
-		}catch(FileNotFoundException e){
-			createFile();
-			br = new BufferedReader(new FileReader(REG_ID_STORE));
-		}
-		
-		String regId = "";
-		Set<String> regIdSet = new HashSet<String>();
-		while ((regId = br.readLine()) != null) {
-			regIdSet.add(regId);
-		}
-		br.close();
-		return regIdSet;
-	}
+	static Connection con;
 	
-	public static void createFile(){
-		 try {
-	            // Assume default encoding.
-	            FileWriter fileWriter =
-	                new FileWriter(REG_ID_STORE);
-	            // Always wrap FileWriter in BufferedWriter.
-	            BufferedWriter bufferedWriter =
-	                new BufferedWriter(fileWriter);
-	            
-	            bufferedWriter.write("RegistrationFile");
-	            // Always close files.
-	            bufferedWriter.close();
-	        }
-	        catch(IOException ex) {
-	            System.out.println(
-	                "Error writing to file '" + REG_ID_STORE + "'");
-	            // ex.printStackTrace();
-	        }
+	public static void writeToFile(String pNo, String regId){
+
+        PreparedStatement ps;
+		try {
+			dbConnect();
+			ps = con.prepareStatement("INSERT INTO chatServer.RegTable(Mobile, RegId) VALUES (?,?)");
+
+	        ps.setString(1, pNo);
+	        ps.setString(2, regId);
+	        ps.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static Set<String> readFromFile(String mobi) throws SQLException{
+		dbConnect();
+		Set<String> regIdSet = new HashSet<String>();
+		String query = "Select * from chatServer.RegTable WHERE Mobile =" + mobi;
+
+        Statement s = con.createStatement();
+        ResultSet rs = s.executeQuery(query);
+        
+        while (rs.next()) {
+			regIdSet.add(rs.getString(1));
+		}
+		return regIdSet;
 		
+		}
+ 	
+	
+	/**	
+	 * creating database connection
+	 **/
+	protected static void dbConnect(){
+		try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("loaded class");
+            con = DriverManager.getConnection("jdbc:hsqldb:\\ChatServerDual\\db_script", "root", "");
+            System.out.println("created con");
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
 	}
 }
